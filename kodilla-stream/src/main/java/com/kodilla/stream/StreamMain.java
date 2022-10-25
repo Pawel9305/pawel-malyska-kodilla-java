fb107a0121fc7defb7cd070434e317db0e2d5394
pawelmalyska <pawel.malyska93@gmail.com>
2022-10-25 22:41:04 +0200

    7.1 Text beautifier

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,13 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
