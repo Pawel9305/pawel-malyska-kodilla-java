7ddfd3d867518772539496f747ae180ceed2c2c6
pawelmalyska <pawel.malyska93@gmail.com>
2022-10-25 22:43:20 +0200

    7.1 task

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,11 +1,16 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
 
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Tekst", text -> "abc".toUpperCase() + text + "abc".toUpperCase());
+        poemBeautifier.beautify("STRING TO LOWER CASE", text -> text.toLowerCase());
+        poemBeautifier.beautify("text", text -> "ABC >".toLowerCase() + text + "< ABC".toLowerCase());
 
 
 
