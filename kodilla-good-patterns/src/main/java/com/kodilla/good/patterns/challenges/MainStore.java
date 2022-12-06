a1b185b3d137d5041662bf27a9948b6efd133397
pawelmalyska <pawel.malyska93@gmail.com>
2022-12-06 19:35:06 +0100

    task 13.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MainStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MainStore.java
@@ -1,2 +1,13 @@
-package com.kodilla.good.patterns.challenges;public class MainStore {
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class MainStore {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        String translations = movieStore.getMovies().entrySet().stream()
+                .flatMap(e -> e.getValue().stream()).
+                collect(Collectors.joining("!"));
+        System.out.println(translations);
+    }
 }
